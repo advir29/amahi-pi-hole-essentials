7830e727058c00ea9a59970d221a7b0822f0a487
Aditya Singh <linkadityasingh@gmail.com>
2019-06-28 15:51:28 +0530

    change 1

--- installer.sh
+++ installer.sh
@@ -6,7 +6,6 @@ mv /usr/bin/hda-ctl /usr/bin/hda-ctl.orig
 cp hda-ctl /usr/bin/hda-ctl
 chmod --reference=/usr/bin/hda-ctl.orig /usr/bin/hda-ctl
 systemctl restart hda-ctl
-rm /etc/dnsmasq.d/amahi-*
 sed -i '/DocumentRoot/c\        DocumentRoot /var/hda/web-apps/pihole/html/admin' /etc/httpd/conf.d/*pihole.conf
 sed -i '/<Directory/c\        <Directory "/var/hda/web-apps/pihole/html/admin">' /etc/httpd/conf.d/*pihole.conf
 ./basic-install.sh -d /var/hda/web-apps/pihole &
