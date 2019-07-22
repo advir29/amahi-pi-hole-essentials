63d916aafb06fe0526a3f7138789d9cc6d022f4f
Aditya Singh <linkadityasingh@gmail.com>
2019-07-23 00:44:23 +0530

    restart httpd removed

--- installer.sh
+++ installer.sh
@@ -8,5 +8,4 @@ chmod --reference=/usr/bin/hda-ctl.orig /usr/bin/hda-ctl
 systemctl restart hda-ctl
 sed -i '/DocumentRoot/c\        DocumentRoot /var/hda/web-apps/pihole/html/admin' /etc/httpd/conf.d/*pihole.conf
 sed -i '/<Directory/c\        <Directory "/var/hda/web-apps/pihole/html/admin">' /etc/httpd/conf.d/*pihole.conf
-sudo systemctl restart httpd
-./basic-install.sh -d /var/hda/web-apps/pihole
+./basic-install.sh -d /var/hda/web-apps/pihole &
